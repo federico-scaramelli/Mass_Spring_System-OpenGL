9964a9ebcdfb90f101aba86c4afd8af58e28380a
IvanJackJack <ivancimino@hotmail.it>
2022-08-12 21:26:35 +0200

    Sphere as default mesh of gameobjects

--- Mass_Spring_System-OpenGL/Mass_Spring_System-OpenGL/src/engine/GameObject.h
+++ Mass_Spring_System-OpenGL/Mass_Spring_System-OpenGL/src/engine/GameObject.h
@@ -20,7 +20,8 @@ public:
 
 	void SetDefaultMesh ()
 	{
-		SetCubeMesh();
+		// SetCubeMesh();
+		SetSphereMesh();
 	}
 
 	GameObject(const char* name) : name(name)
@@ -121,5 +122,81 @@ public:
 			5, 4, 7,
 			7, 6, 5
 		};
+	}
+
+	void SetSphereMesh()
+	{
+		//Vertici
+		auto& vertices = m_Mesh.GetVertices();
+		vertices.clear();
+
+		float PI=3.14f;
+		float radius=0.5f;
+		int stackCount=8;
+		int sectorCount=stackCount*3;
+		
+		float x, y, z, xy;                              // vertex position
+		float nx, ny, nz, lengthInv = 1.0f / radius;    // vertex normal
+
+		float sectorStep = 2 * PI / sectorCount;
+		float stackStep = PI / stackCount;
+		float sectorAngle, stackAngle;
+
+		for(int i = 0; i <= stackCount; ++i)
+		{
+		    stackAngle = PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
+		    xy = radius * cosf(stackAngle);          // r * cos(u)
+		    z = radius * sinf(stackAngle);           // r * sin(u)
+			
+		    for(int j = 0; j <= sectorCount; ++j)
+		    {
+				Vertex vertex{};
+
+		        sectorAngle = j * sectorStep;           // starting from 0 to 2pi
+				
+		        x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
+		        y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
+
+				vertex.position={x,y,z};
+				
+		        nx = x * lengthInv;
+		        ny = y * lengthInv;
+		        nz = z * lengthInv;
+
+				vertex.normal={nx,ny,nz};
+
+				vertices.push_back(vertex);
+		    }
+		}
+
+		//Indici
+		auto& indices = m_Mesh.GetIndices();
+		indices.clear();
+		
+		int k1, k2;
+		for(int i = 0; i < stackCount; ++i)
+		{
+		    k1 = i * (sectorCount + 1);     // beginning of current stack
+		    k2 = k1 + sectorCount + 1;      // beginning of next stack
+
+		    for(int j = 0; j < sectorCount; ++j, ++k1, ++k2)
+		    {
+		        // k1 => k2 => k1+1
+		        if(i != 0)
+		        {
+		            indices.push_back(k1);
+		            indices.push_back(k2);
+		            indices.push_back(k1 + 1);
+		        }
+
+		        // k1+1 => k2 => k2+1
+		        if(i != (stackCount-1))
+		        {
+		            indices.push_back(k1 + 1);
+		            indices.push_back(k2);
+		            indices.push_back(k2 + 1);
+		        }
+		    }
+		}
 	}
 };
